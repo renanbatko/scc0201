dec301e2e62ed38421320999d1e210efbd924360
renanbatko <renanbatkousp@gmail.com>
2014-08-25 22:20:51 -0300

    matriz aumentada ok

--- trabalho02/t2.c
+++ trabalho02/t2.c
@@ -1,11 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+//funcao apenas para testes
 void imprimetudo(double **mat, double *vet, int dim){
 	int i, j;
 	printf("Matriz: \n");
 	for (i = 0; i < dim; i++){
-		for (j = 0; j < dim; j++){
+		for (j = 0; j < dim+1; j++){
 			printf("%.5lf ", mat[i][j]);
 		}
 		printf("\n");
@@ -27,7 +28,7 @@ int main(void){
 	double **mat;
 	mat = (double **) malloc(dim * sizeof(double *));
 	for (i = 0; i < dim; i++){
-		mat[i] = (double *) malloc(dim * sizeof(double));
+		mat[i] = (double *) malloc((dim+1) * sizeof(double));
 	}
 	
 	//leitura dos elementos da matriz
@@ -47,6 +48,20 @@ int main(void){
 	}
 	
 	imprimetudo(mat, vet, dim);
+	
+	//matriz aumentada
+	for (i = 0; i < dim; i++){
+		mat[i][dim] = vet[i];
+	}
+	
+	imprimetudo(mat, vet, dim);
+	
+	//liberacao de memoria
+	free(vet);
+	for (i = 0; i < dim; i++){
+		free(mat[i]);
+	}
+	free(mat);
 	
 	return 0;
 }
