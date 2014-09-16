4210dc81cbbd04e1686b60603960bc8adb38fc35
renanbatko <renanbatkousp@gmail.com>
2014-09-15 21:27:24 -0300

    não está funcionando

--- trabalho03/t3.c
+++ trabalho03/t3.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 #include <math.h>
 
-void imprime_mat(double **mat, double *vet, int n){
+//funcao apenas para testes
+/*void imprime(double **mat, double *vet, int n){
 	int i, j;
 	for (i = 0; i < n; i++){
 		for (j = 0; j < 3; j++){
@@ -15,7 +16,7 @@ void imprime_mat(double **mat, double *vet, int n){
 	
 	for (i = 0; i < n; i++)
 		printf("%lf ", vet[i]);
-}
+}*/
 
 void gera_eq(double x, double *a, double *b, double *c, double *d){
 	//y = ax^3 + bx^2 + cx + d
@@ -23,6 +24,28 @@ void gera_eq(double x, double *a, double *b, double *c, double *d){
 	*b = pow(x, 2);
 	*c = x;
 	*d = 1;
+}
+
+void gauss(double **mat, double *vet, double *x, int dim){
+	int k, i, j;
+	for (k = 0; k < dim-1; k++){
+		for (i = k+1; i < dim; i++){
+			for (j = k+1; j < dim; j++){
+				mat[i][j] = mat[i][j] - ((mat[i][k] * mat[k][j])/(mat[k][k]));
+			}
+			vet[i] = vet[i] - ((mat[i][k] * vet[k])/(mat[k][k]));
+		}
+	}
+	
+	x[dim-1] = vet[dim-1]/mat[dim-1][dim-1];
+
+	for (i = dim-2; i >= 0; i--){
+		double s = 0;
+		for (j = i+1; j < dim; j++){
+			s = s + mat[i][j] * x[j];
+		}
+		x[i] = (vet[i] - s)/mat[i][i];
+	}
 }
 
 int main(void){
@@ -58,7 +81,7 @@ int main(void){
 		//if (i != 0)
 		vet[k] = v[i+1];
 		gera_eq(v[i], &abcd[0], &abcd[1], &abcd[2], &abcd[3]);
-		printf("a = %lf b = %lf c = %lf d = %lf\n", abcd[0], abcd[1], abcd[2], abcd[3]);
+		//printf("a = %lf b = %lf c = %lf d = %lf\n", abcd[0], abcd[1], abcd[2], abcd[3]);
 		for (j = 0; j < n; j++){
 			mat[jj][j] = abcd[j];
 		}
@@ -66,7 +89,18 @@ int main(void){
 		k++;
 	}
 	
-	imprime_mat(mat, vet, n);
+	//alocacao do vetor x
+	double *x;
+	x = (double *) malloc(n * sizeof(double));
+	
+	gauss(mat, vet, x, n);
+	
+	//impressao dos resultados
+	for (i = 0; i < n; i++){
+		printf("%.6lf\n", x[i]);
+	}
+	
+	//imprime(mat, vet, n);
 	free(v);
 	free(vet);
 	free(mat);	
