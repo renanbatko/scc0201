f8fc2c1e95298ba31032714e7297e58bf555a642
renanbatko <renanbatkousp@gmail.com>
2014-10-28 23:24:28 -0200

    desenvolvendo t6

--- trabalho06/t6.c
+++ trabalho06/t6.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#define PI 3.14159265
+
+void dct2(unsigned char *bytes, float *new_bytes, int size) {
+	int i, k;
+	for (k = 0; k < size; k++) {
+		new_bytes[k] = 0;
+		for (i = 0; i < size; i++) {
+			new_bytes[i] += ((bytes[i] * 1.0) * cos((PI/size) * (i + 0.5) * k));
+		}
+	}
+}
 
 int main(int argc, char *argv[]) {
 	char audio_name[20];
@@ -21,9 +32,17 @@ int main(int argc, char *argv[]) {
 		counter++;
 		bytes = (unsigned char *) realloc(bytes, (counter + 1) * sizeof(unsigned char));
 	}
+	int size;
+	size = counter;
 	
+	float *new_bytes;
+	new_bytes = (float *) malloc((size + 1) * sizeof(float));
+	
+	dct2(bytes, new_bytes, size);
 	
 	free(bytes);
+	free(new_bytes);
 	
 	return 0;
 }
+
