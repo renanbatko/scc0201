b33102af5f12a45aa8e6bcda2abf19a1a57680ca
renanbatko <renanbatkousp@gmail.com>
2014-11-09 16:12:16 -0200

    problemas com o index

--- trabalho07/main.c
+++ trabalho07/main.c
@@ -9,13 +9,48 @@ char *next_command(FILE *stream) {
 	
 	do {
 		ch = fgetc(stream);
-		buffer = (char *) realloc(buffer, (counter+1) * sizeof(char));
+		buffer = (char *) realloc(buffer, (counter + 1) * sizeof(char));
 		counter++;
 		buffer[counter-1] = ch;
 	} while (ch != '\n');
-	buffer[counter-1] = '\0';
+	counter++;
+	buffer = (char *) realloc(buffer, (counter + 1) * sizeof(char));
+	buffer[counter-1] = ' ';
+	buffer[counter] = '\0';
 	
 	return buffer;
+}
+
+void verify_command(char *full_command) {
+	if (!strcmp(full_command, "-lixo")) return;
+	char *command = NULL;
+	int i = 0;
+	
+	//printf("comando full: %s\n", full_command);
+	while (full_command[i] != ' ') {
+		//printf("%c", full_command[i]);
+		command = (char *) realloc(command, (i + 1) * sizeof(char));
+		command[i] = full_command[i];
+		i++;
+	}
+	command[i] = '\0';
+	
+	int temp = strcmp(command, "index");
+	printf("temp = %d\n", temp);
+	printf("command: %s\n", command);
+	if (!strcmp(command, "insert")) {
+		printf("insert\n");
+	}
+	
+	if (!strcmp(command, "index")) {
+		printf("index\n");
+	}
+	
+	if (!strcmp(command, "search")) {
+		printf("search\n");
+	}
+	
+	free(command);
 }
 
 int main(int argc, char *argv[]) {
@@ -26,17 +61,25 @@ int main(int argc, char *argv[]) {
 	//mfp = fopen(filename, "r");
 	//if (mfp == NULL) exit(1);
 	
+	int flag = 0;
 	while (1) {
 		char *command;
 		command = next_command(stdin);
-		printf("%s", command);
+		if (flag == 0) {
+			strcpy(command, "-lixo");
+			flag++;
+		}
+		//printf("%s", command);
 		
 		//int comp = strcmp(command, "exit");
 		//printf("comp = %d\n", comp);
-		if (!strcmp(command, "exit")) {
+		if (!strcmp(command, "exit ")) {
 			free(command);
 			break;
 		}
+		//printf("antes\n");
+		verify_command(command);
+		//printf("depois\n");
 	}
 	
 	return 0;
