fab8ba30b62596b377e995a535eb7891f4f47903
renanbatko <renanbatkousp@gmail.com>
2014-11-11 20:36:03 -0200

    terminando verify command

--- trabalho07/main.c
+++ trabalho07/main.c
@@ -17,10 +17,42 @@ char *read_command() {
 	return command;
 }
 
+int verify_command(char *full_command) {
+	char *command = NULL;
+	int counter = 0, i = 0;
+	//printf("full_command: %s\n", full_command);
+	while (full_command[i] != ' ' && full_command[i] != '\0') {
+		//printf("full_command[i] = %c\n", full_command[i]);
+		command = (char *) realloc(command, (counter+1) * sizeof(char));
+		command[counter] = full_command[i];
+		counter++;
+		i++;
+	}
+	command[counter] = '\0';
+	
+	//printf("COMANDO: %s\n", command);
+	
+	return 1;
+}
+
 int main(int argc, char *argv[]) {
 	char *command = read_command();
 	
-	//printf("comando: %s\n", command);
+	verify_command(command);
+	/*switch (verify_command(command)) {
+		case 1:
+			//something something
+		break;
+		case 2:
+			//something something
+		break;
+		case 3:
+			//something something
+		break;
+		case 4:
+			//something something
+		break;
+	}*/
 	
 	free(command);
 	return 0;
